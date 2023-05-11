e74aae75cf2448be0fa7561c9e3beeb366162cf1
“Maria <marianicolae24@yahoo.com>
2023-05-11 20:56:49 +0300

    Issue with the Keys.ENTER Thread.sleep() to resolve

--- java-automation/src/test/java/tests/GoogleUITest.java
+++ java-automation/src/test/java/tests/GoogleUITest.java
@@ -1,7 +1,10 @@
 package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -16,9 +19,21 @@ public class GoogleUITest {
         WebDriverManager.chromedriver().setup();
     }
     @Test
-    public void SearchOnGoogleTest() {
+    public void SearchOnGoogleTest() throws InterruptedException{
         driver = new ChromeDriver();
         driver.get("http://google.com");
+
+        WebElement acceptCookieButton = driver.findElement(By.id("L2AGLb"));
+        acceptCookieButton.click();
+
+        WebElement searchInput = driver.findElement(By.name("q")) ;
+        searchInput.sendKeys("Google Atelierul Digital");
+        Thread.sleep(100);
+        searchInput.sendKeys(Keys.ENTER);
+
+
     }
+
+
 
 }
